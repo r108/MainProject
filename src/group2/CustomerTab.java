c46917f694a9d429f58d1a7aa091b980962a2110
John o k <ditgroup2@github>
2014-08-14 14:08:33 +0100

    test

--- src/group2/CustomerTab.java
+++ src/group2/CustomerTab.java
@@ -1,13 +1,28 @@
 package group2;
 
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class CustomerTab extends JPanel{
-
+	public ArrayList<Customer> customerList;
 	/**
 	 * Create the panel.
 	 */
-	public CustomerTab() {
-	
+	public CustomerTab(ArrayList<Customer> customerList) {
+		this.customerList = customerList;
+		setLayout(null);
+		
+		JButton buttonCu = new JButton("New button");
+		buttonCu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		buttonCu.setBounds(28, 81, 117, 29);
+		add(buttonCu);
 	}
 }
