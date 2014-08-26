80d2bd739732943e0c1f72ce4b3116990608f815
Conor Clarke <con.clarke3@gmail.com>
2014-08-26 12:19:03 +0100

    Full screen

--- src/guiTabs/MainGUI.java
+++ src/guiTabs/MainGUI.java
@@ -1,18 +1,14 @@
 package guiTabs;
 
-
-
-
 import java.awt.SystemColor;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -27,10 +23,8 @@ import javax.swing.border.EmptyBorder;
 import order.StockDBControl;
 import retailSystem.Login;
 import retailSystem.PersonDB;
-import retailSystem.Product;
 
-
-public class MainGUI extends JFrame implements ActionListener{
+public class MainGUI extends JFrame implements ActionListener {
 
 	private JTabbedPane tabbedPane;
 	private PurchaseOrderTab purchaseOrderTab;
@@ -56,8 +50,7 @@ public class MainGUI extends JFrame implements ActionListener{
 	private File file;
 	private BufferedWriter writer;
 	private StringBuilder stringBuilder;
-	
-	
+
 	public File getFile() {
 		return file;
 	}
@@ -85,34 +78,40 @@ public class MainGUI extends JFrame implements ActionListener{
 	public ProductTab getProductTab() {
 		return productTab;
 	}
-	
+
 	public CustomerTab getCustomerTab() {
 		return customerTab;
 	}
-	
+
 	public StaffTab getStaffTab() {
 		return staffTab;
 	}
 
 	private JPanel contentPane;
-	
-	public MainGUI(PersonDB personDB, StockDBControl stockDBControl,Login login){
+
+	public MainGUI(PersonDB personDB, StockDBControl stockDBControl, Login login) {
+		Toolkit tk = Toolkit.getDefaultToolkit();
+
 		this.login = login;
 		this.stockDBControl = stockDBControl;
 		this.setVisible(true);
-		setSize(560,450);
+
+		int xSize = ((int) tk.getScreenSize().getWidth());
+		int ySize = ((int) tk.getScreenSize().getHeight());
+		this.setSize(xSize, ySize);
+
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		contentPane = new JPanel();
 		setContentPane(contentPane);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));		
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(null);
-		
+
 		fileChooser = new JFileChooser();
 		fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-		
-		
+
+		// Create menu items
 		menuBar = new JMenuBar();
 		fileMenu = new JMenu("File", true);
 		userMenu = new JMenu("User", true);
@@ -123,120 +122,142 @@ public class MainGUI extends JFrame implements ActionListener{
 		saveMenuItem = new JMenuItem("Save");
 		aboutMenuItem = new JMenuItem("About Group2");
 		howToMenuItem = new JMenuItem("User Guide");
-		
+
+		// Add menu items to their relevant menu
 		fileMenu.add(openMenuItem);
 		fileMenu.add(saveMenuItem);
 		userMenu.add(logoutMenuItem);
 		userMenu.add(exitMenuItem);
 		helpMenu.add(howToMenuItem);
 		helpMenu.add(aboutMenuItem);
-		
+
+		// Add the menus to the menu bar
 		menuBar.add(fileMenu);
 		menuBar.add(userMenu);
 		menuBar.add(helpMenu);
 		setJMenuBar(menuBar);
-		
+
+		// Append listeners for the menu items
 		logoutMenuItem.addActionListener(this);
 		exitMenuItem.addActionListener(this);
 		openMenuItem.addActionListener(this);
 		saveMenuItem.addActionListener(this);
 		aboutMenuItem.addActionListener(this);
 		howToMenuItem.addActionListener(this);
-		
-		
+
+		// Set up a tabbed pane system
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBackground(SystemColor.window);
-		tabbedPane.setBounds(10, 11, 532, 381);
+		tabbedPane.setBounds(10, 10, xSize, ySize);
 		contentPane.add(tabbedPane);
 
-		supplierTab = new SupplierTab(personDB,tabbedPane,this);
+		// Set up the tabs, passing in the relevant data structures
+		supplierTab = new SupplierTab(personDB, tabbedPane, this);
 		staffTab = new StaffTab(personDB);
 		customerTab = new CustomerTab(personDB);
-		productTab = new ProductTab(stockDBControl,personDB,tabbedPane,supplierTab,this);
+		productTab = new ProductTab(stockDBControl, personDB, tabbedPane,
+				supplierTab, this);
 		stockControlTab = new StockControlTab(stockDBControl);
 		customerOrderTab = new CustomerOrderTab(stockDBControl);
-		//purchaseOrderTab  = new PurchaseOrderTab(stockDBControl, personDB);
-		
+		// purchaseOrderTab = new PurchaseOrderTab(stockDBControl, personDB);
+
+		// Add the tabs to the pane
 		tabbedPane.addTab("Customer", null, customerTab, null);
 		tabbedPane.addTab("Supplier", null, supplierTab, null);
 		tabbedPane.addTab("Staff", null, staffTab, null);
 		tabbedPane.addTab("Product", null, productTab, null);
-		//tabbedPane.addTab("Purchase", null, purchaseOrderTab, null);
+		// tabbedPane.addTab("Purchase", null, purchaseOrderTab, null);
 		tabbedPane.addTab("Customer Order", null, customerOrderTab, null);
 		tabbedPane.addTab("Stock Control", null, stockControlTab, null);
-		
 	}
 
+	// Action events for the different menu items
+	// Note: It's bad practice to do action listeners in this way, even though
+	// it
+	// shortens the code.
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		if (event.getActionCommand().equals("Exit")){
-			
-			int answer=JOptionPane.showConfirmDialog(null, "Are you sure you want to exit the program?", " CONFIRMATION " , JOptionPane.YES_NO_OPTION);
-			if ( answer==JOptionPane.YES_OPTION ){
+
+		// Exit option
+		if (event.getActionCommand().equals("Exit")) {
+
+			int answer = JOptionPane.showConfirmDialog(null,
+					"Are you sure you want to exit the program?",
+					" CONFIRMATION ", JOptionPane.YES_NO_OPTION);
+			if (answer == JOptionPane.YES_OPTION) {
 				System.exit(0);
 			}
 			System.out.println("Terminate Program!");
 		}
-		if (event.getActionCommand().equals("Logout")){
+
+		// Logout option
+		if (event.getActionCommand().equals("Logout")) {
 			this.setVisible(false);
 			login.clearFields();
 			login.setVisible(true);
-			
+
 			supplierTab.cancelButton.doClick();
 			staffTab.cancelButton.doClick();
 			customerTab.cancelButton.doClick();
 			productTab.cancelButton.doClick();
-			
+
 			supplierTab.cancelEditButton.doClick();
 			staffTab.cancelEditButton.doClick();
 			customerTab.cancelEditButton.doClick();
 			productTab.cancelEditButton.doClick();
-			
+
 			tabbedPane.setSelectedComponent(customerTab);
-			
 		}
-		if (event.getActionCommand().equals("About Group2")){
-			
-			JOptionPane.showMessageDialog(null, "DIT FCP-2014 GROUP 2 MEMBERS\n\nConor Clarke\nPeter Farrel\nJohn Fleming\nSzabolcs Hutvagner\nJohn O`Keeffe\nRoland Katona");
-			
+
+		// About option
+		if (event.getActionCommand().equals("About Group2")) {
+			String message = "DIT FCP-2014 GROUP 2 MEMBERS\n\n"
+					+ "Conor Clarke\n" + "Peter Farrel\n" + "John Fleming\n"
+					+ "Szabolcs Hutvagner\n" + "John O`Keeffe\n"
+					+ "Roland Katona";
+			JOptionPane.showMessageDialog(null, message);
 		}
-		
-		if (event.getActionCommand().equals("Open")){
-			
-            if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION)
-            {
-                file = fileChooser.getSelectedFile();
-            }	
+
+		// About option
+		if (event.getActionCommand().equals("Open")) {
+			if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+				file = fileChooser.getSelectedFile();
+			}
 		}
-		if (event.getActionCommand().equals("Save")){
-			if (fileChooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION)
-			{
-				String str ="ERFERERERE";
-				/*try(FileWriter fw = new FileWriter(fileChooser.getSelectedFile()+".txt")) {
-				    fw.write(sb.toString());
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				file = fileChooser.getSelectedFile();*/
+
+		// Save option (need to edit)
+		if (event.getActionCommand().equals("Save")) {
+			if (fileChooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+				String str = "ERFERERERE";
+				/*
+				 * try(FileWriter fw = new
+				 * FileWriter(fileChooser.getSelectedFile()+".txt")) {
+				 * fw.write(sb.toString()); } catch (IOException e) { //
+				 * Auto-generated catch block e.printStackTrace(); } file =
+				 * fileChooser.getSelectedFile();
+				 */
 				stringBuilder = new StringBuilder();
-				productTab.buildProductDetailsString(stockDBControl.getStockList());
-				
+				productTab.buildProductDetailsString(stockDBControl
+						.getStockList());
+
 				try {
-				    writer = new BufferedWriter(new OutputStreamWriter(
-				          new FileOutputStream(fileChooser.getSelectedFile()+".txt"), "utf-8"));
-				    writer.write("Something "+stringBuilder);
+					writer = new BufferedWriter(new OutputStreamWriter(
+							new FileOutputStream(fileChooser.getSelectedFile()
+									+ ".txt"), "utf-8"));
+					writer.write("Something " + stringBuilder);
 				}
 				catch (IOException e) {
 					e.printStackTrace();
 				}
 				finally {
-				    try {writer.close();} catch (Exception e) {e.printStackTrace();}
+					try {
+						writer.close();
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+					}
 				}
-				
-				
-				
 			}
-		}	
+		}
 	}
 }
--- src/retailSystem/Login.java
+++ src/retailSystem/Login.java
@@ -2,32 +2,32 @@ package retailSystem;
 
 import guiTabs.MainGUI;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 import order.StockDBControl;
 import person.Person;
 import person.Staff;
 
 /**
- * Launches a GUI for entering a username and password, with
- * password validation taking place within the Staff class.
+ * Launches a GUI for entering a username and password, with password validation
+ * taking place within the Staff class.
+ * 
  * @author Conor Clarke, modified by Roland
  */
 @SuppressWarnings("serial")
-public class Login extends JFrame implements ActionListener{
-	
+public class Login extends JFrame implements ActionListener {
+
 	private PersonDB personDB;
 	private StockDBControl stockDBControl;
 	private ArrayList<Person> staffList;
@@ -36,114 +36,110 @@ public class Login extends JFrame implements ActionListener{
 	private JTextField usernameField;
 	private JPasswordField passwordField;
 	private MainGUI gui;
+
 	/**
 	 * Launches the window for the login screen
 	 */
-	public Login(PersonDB personDB,StockDBControl stockDBControl){
+	public Login(PersonDB personDB, StockDBControl stockDBControl) {
 		this.stockDBControl = stockDBControl;
 		this.staffList = personDB.getStaffList();
 		this.personDB = personDB;
 		runLogin();
 		login = false;
-		JOptionPane.showMessageDialog(null, "For staff login use 'Jim/pass'\nFor manager login use 'John/password'");
 	}
-	
-	public void clearFields(){
+
+	public void clearFields() {
 		usernameField.setText("");
 		passwordField.setText("");
 	}
-	
-	public void runLogin(){
-		//Setup code for the JFrame and Panel
+
+	public void runLogin() {
+		// Setup code for the JFrame and Panel
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize( 459, 339);
+		setSize(459, 339);
 		JPanel contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		//Setup code for the username text field
+
+		// Setup code for the username text field
 		usernameField = new JTextField();
 		usernameField.setBounds(162, 84, 86, 20);
 		contentPane.add(usernameField);
 		usernameField.setColumns(10);
-		
-		//Label for the username
+
+		// Label for the username
 		JLabel userName = new JLabel("Username");
 		userName.setBounds(162, 59, 86, 14);
 		contentPane.add(userName);
-		
-		//Setup code for the password field
+
+		// Setup code for the password field
 		passwordField = new JPasswordField();
 		passwordField.setBounds(162, 140, 86, 20);
 		contentPane.add(passwordField);
-		
-		//Label for the password
+
+		// Label for the password
 		JLabel passwordLabel = new JLabel("Password");
 		passwordLabel.setBounds(162, 115, 86, 14);
 		contentPane.add(passwordLabel);
-		
-		//Login button and event handler
+
+		// Login button and event handler
 		btnNewButton = new JButton("Login");
 		contentPane.getRootPane().setDefaultButton(btnNewButton);
 		btnNewButton.addActionListener(this);
-		//Add the login button to the panel
+
+		// Add the login button to the panel
 		btnNewButton.setBounds(162, 205, 89, 23);
 		contentPane.add(btnNewButton);
 		setLocationRelativeTo(null);
 		setVisible(true);
-		gui = new MainGUI(personDB,stockDBControl, this);  
-		gui.setVisible(false);
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==btnNewButton){
-			String	usernameText = usernameField.getText();
+		if (e.getSource() == btnNewButton) {
+			String usernameText = usernameField.getText();
 			Staff user = null;
-			for(Person person:staffList){
-				if(person.getName().equals(usernameText)){
-					user = (Staff)person;
+			for (Person person : staffList) {
+				if (person.getName().equals(usernameText)) {
+					user = (Staff) person;
 					break;
-				}	
+				}
 			}
-			if(user != null){
-				char [] validate = passwordField.getPassword();
-				login = ((Staff)user).passwordValidation(validate);
-				if(login){
-					if(((Staff)user).getAccessLevel()==2){
+			if (user != null) {
+				char[] validate = passwordField.getPassword();
+				login = ((Staff) user).passwordValidation(validate);
+				if (login) {
+					if (((Staff) user).getAccessLevel() == 2) {
+						gui = new MainGUI(personDB, stockDBControl, this);
 						gui.getSupplierTab().enableButtons(true);
 						gui.getCustomerTab().enableButtons(true);
 						gui.getStaffTab().enableButtons(true);
 						gui.getProductTab().enableButtons(true);
-						
 						RetailSystemDriver.setPriviledged(true);
-						System.out.println("Priviledged access granted!");
+						gui.setVisible(true);
+						setVisible(false);
 					}
-					else{
+					else {
+						gui = new MainGUI(personDB, stockDBControl, this);
 						gui.getSupplierTab().enableButtons(false);
 						gui.getCustomerTab().enableButtons(false);
 						gui.getStaffTab().enableButtons(false);
 						gui.getProductTab().enableButtons(false);
-						
 						RetailSystemDriver.setPriviledged(false);
-						System.out.println("Ordinary User Access!");
+						gui.setVisible(true);
+						setVisible(false);
 					}
-					
-					JOptionPane.showMessageDialog(null,"Login successful!");
-					/*if(gui == null)
-						gui = new GUI(personDB,stockControl, this);  */ 
-					gui.setVisible(true);
+
 					setVisible(false);
-					//dispose();	  
+					JOptionPane.showMessageDialog(null, "Login successful!");
 				}
 				else
-					JOptionPane.showMessageDialog(null,"Wrong Password!");
+					JOptionPane.showMessageDialog(null, "Wrong Password!");
 			}
-			else{
-				JOptionPane.showMessageDialog(null,"Invalid username!");
-			}		
+			else
+				JOptionPane.showMessageDialog(null, "Invalid username!");
 		}
-		
 	}
 }
-	
